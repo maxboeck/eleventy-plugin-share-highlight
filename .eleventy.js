27d197ba4a9257c75f35ddba701e591bfc437c33
Max Böck <hello@mxb.at>
2021-04-15 14:28:10 +0200

    Revert "remove share highlights in non-html output"
    This reverts commit 89471b3ca57c51ca02604009512ff6e367071c00.

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 // Example use for the plugin:
 // {% highlight %}Hi Everyone! This is a piece of text that can be shared.{% endhighlight %}
 
-const { JSDOM } = require('jsdom')
-
 module.exports = (eleventyConfig, options) => {
     eleventyConfig.addPairedShortcode('highlight', (content) => {
         const defaults = {
@@ -11,31 +9,4 @@ module.exports = (eleventyConfig, options) => {
         const { label } = Object.assign({}, defaults, options)
         return `<share-highlight aria-label="${label}"><mark>${content}</mark></share-highlight>`
     })
-
-    eleventyConfig.addTransform(
-        'strip-share-highlight',
-        (content, outputPath) => {
-            if (outputPath && !outputPath.endsWith('.html')) {
-                const dom = new JSDOM(content)
-                const { document } = dom.window
-                const shareHighlights = Array.from(
-                    document.querySelectorAll('share-highlight')
-                )
-                const unwrapElement = (el) => {
-                    if (el.firstChild) {
-                        while (el.firstChild) {
-                            el.parentNode.insertBefore(el.firstChild, el)
-                        }
-                        el.parentNode.removeChild(el)
-                    }
-                }
-                if (shareHighlights.length) {
-                    shareHighlights.forEach(unwrapElement)
-                    return dom.serialize()
-                }
-            }
-
-            return content
-        }
-    )
 }
